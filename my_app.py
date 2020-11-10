b1fd3b28e173c3d8e10ed117a631dd549608105f
Bjarke <bjarke.hastrup@gmail.com>
2020-11-10 19:06:27 +0100

    First commit

---
+++ my_app.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+import datetime
+import numpy as np
+
+from typing import Dict, Tuple, List, Set, Union, Any, cast
+
+import streamlit as st
+
+
+
+project_list = [None] + ['SPSA', 
+		     'RL for Doom',
+		     'MPC for algo bidding',
+		     'Model Based Machine Learning for soccer']
+
+st.sidebar.header('Choose project:')
+agency = st.sidebar.selectbox('Agency', project_list)
+
+# make function that reads project abstracts
